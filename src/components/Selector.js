ff01289eb48696b5227a8534ab955248606364cd
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-22 14:53:13 -0400

    successfully updated state more than once planning seperating task into more function

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,31 +1,65 @@
 import React, { Component } from 'react'
+import Converter from './Converter'
+
 
 export class Selector extends Component {
 
 state={
     country:'',
-    amount:''
+    amount:0,
+    conversion:0
 }
+
+//////////////////////
+
    onSubmit = (event) => {
-    event.preventDefault();
-    console.log("SELECTOR IS INN",this.state.amount)
+     event.preventDefault();
+    this.setState({[event.target.name]: event.target.value })
+    // console.log("SELECTOR IS INN",this.state.country)
+    // console.log("SELECTOR IS INN",this.state.amount)
+    //this.props.pick(this.state.country,this.state.amount)
+    //this.setState({ username: '',password:'' });
+   // this.setState({country:this.state.amount})
+    if (this.state.country==="Brazil"){
+        let a=this.state.amount/this.props.rates.DOP
+        this.setState({conversion:a})
+        console.log("new amnt",this.state.conversion)
+         return this.state.conversion;
+    }else{
+        console.log("did not update")
+    }
+    
+   
   }
   //////////////////////////
-  onChange = (event) => (this.setState({amount: event.target.value })); 
+ 
+  onChange = (event) => (this.setState({[event.target.name]: event.target.value })); 
 
     render() {
         return (
             <div>
+                
+
                 <form className="forms" onSubmit={this.onSubmit} >
-                    <label id="abc">Currency: </label>
+                    <div>
+                        <label id="abc">Currency: </label>
                     
-                    <select id="countries" name="countries" onChange={this.onChange}>
-                        <option value="testingvalue">-Select Country-</option>
-                        <option value="Dominican Republic">Dominican Republic</option>
-                        <option value="Brazil">Brazil</option>
-                   </select>
-          
-                    <input id="submit" type="submit"></input>
+                        <select id="countries" name="country" onChange={this.onChange}>
+                            <option value="dummyvalue" >-Select Country-</option>
+                            <option value="Dominican Republic" >Dominican Republic</option>
+                            <option value="Brazil">Brazil</option>
+                        </select>
+                        
+                        Enter amount:
+                        <input type="number" name="amount" placeholder="amount" value={this.state.amount}
+                         onChange={this.onChange}/>
+                    
+                        <input id="submit" type="submit"></input>
+                        <Converter choice={this.state.conversion} 
+                           amount={this.state.amount} 
+                           rates={this.props}/>
+                    </div>
+                  
                 </form>
             </div>
         )
