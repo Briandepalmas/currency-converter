8a9e2c8ba42f6b6632da9fbd7f9ec8d810442fd4
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-22 12:47:35 -0400

    created converter component

---
+++ src/components/Converter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import Selector from './Selector'
+function Converter(props) {
+    
+      
+    console.log("converter-->",props.choice,props.amount,props.rates)
+    return (
+        <div>
+            
+        </div>
+    )
+    
+}
+
+export default Converter
