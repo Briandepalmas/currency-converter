c2ebbbaf074adf7da4c90a2f9ef15888d158ac80
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-24 14:19:52 -0400

    limited rates shown to 5

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -9,82 +9,11 @@ export default function Rates() {
         <div>
             <h1>
                 <ul>
-                <li>"USD": 1</li>
-                <li>"AED": 3.67,</li>
-                <li>"BRL": 5.26,</li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                
-                
-                "ARS": 65.85,
-                "AUD": 1.57,
-                "BGN": 1.8,
-                
-                "BSD": 1,
-                "CAD": 1.41,
-                "CHF": 0.967,
-                "CLP": 851.45,
-                "CNY": 7.07,
-                "COP": 3861.14,
-                "CZK": 25.03,
-                "DKK": 6.86,
-                "DOP": 54,
-                "EGP": 15.72,
-                "EUR": 0.92,
-                "FJD": 2.28,
-                "GBP": 0.803,
-                "GTQ": 7.71,
-                "HKD": 7.75,
-                "HRK": 6.95,
-                "HUF": 324.29,
-                "IDR": 15281.46,
-                "ILS": 3.58,
-                "INR": 76.56,
-                "ISK": 144.04,
-                "JPY": 107.68,
-                "KRW": 1219.09,
-                "KZT": 424.73,
-                "MXN": 24.04,
-                "MYR": 4.37,
-                "NOK": 10.4,
-                "NZD": 1.66,
-                "PAB": 1,
-                "PEN": 3.41,
-                "PHP": 50.81,
-                "PKR": 162.73,
-                "PLN": 4.16,
-                "PYG": 6534.23,
-                "RON": 4.45,
-                "RUB": 74.42,
-                "SAR": 3.75,
-                "SEK": 10,
-                "SGD": 1.42,
-                "THB": 32.52,
-                "TRY": 6.93,
-                "TWD": 30.04,
-                "UAH": 27.07,
-                "UYU": 43.3,
-                "ZAR": 18.78
+                 <li>"USD": 1</li>
+                 <li>"AED": 3.67,</li>
+                 <li>"BRL": 5.26,</li> 
+                 <li>"GBP": 0.803,</li>
+                 <li>"JPY": 107.68,</li>
                 </ul>
                 
             </h1>
