a07d293f8037abde6b74ee170274091464213676
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-24 14:13:08 -0400

    made changes to rates component

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,19 +1,94 @@
 import React from 'react'
 
-export default function Rates(props) {
+export default function Rates() {
 
-
-//for-in code block: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
- 
-         const coinObject=props.rates;
+       
+         
     
     return (
         <div>
-        <h1>{coinObject.BRL + " Brazil"}</h1>
-        <h1>{coinObject.DOP + " Dominican Republic"}</h1>
-        <h1>{coinObject.GBP + " United Kingdom"}</h1>
-        <h1>{coinObject.JPY + " Japan"}</h1>
-        <h1>{coinObject.AED + " United Arab Emirates"}</h1>
+            <h1>
+                <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                "USD": 1,
+                "AED": 3.67,
+                "ARS": 65.85,
+                "AUD": 1.57,
+                "BGN": 1.8,
+                "BRL": 5.26,
+                "BSD": 1,
+                "CAD": 1.41,
+                "CHF": 0.967,
+                "CLP": 851.45,
+                "CNY": 7.07,
+                "COP": 3861.14,
+                "CZK": 25.03,
+                "DKK": 6.86,
+                "DOP": 54,
+                "EGP": 15.72,
+                "EUR": 0.92,
+                "FJD": 2.28,
+                "GBP": 0.803,
+                "GTQ": 7.71,
+                "HKD": 7.75,
+                "HRK": 6.95,
+                "HUF": 324.29,
+                "IDR": 15281.46,
+                "ILS": 3.58,
+                "INR": 76.56,
+                "ISK": 144.04,
+                "JPY": 107.68,
+                "KRW": 1219.09,
+                "KZT": 424.73,
+                "MXN": 24.04,
+                "MYR": 4.37,
+                "NOK": 10.4,
+                "NZD": 1.66,
+                "PAB": 1,
+                "PEN": 3.41,
+                "PHP": 50.81,
+                "PKR": 162.73,
+                "PLN": 4.16,
+                "PYG": 6534.23,
+                "RON": 4.45,
+                "RUB": 74.42,
+                "SAR": 3.75,
+                "SEK": 10,
+                "SGD": 1.42,
+                "THB": 32.52,
+                "TRY": 6.93,
+                "TWD": 30.04,
+                "UAH": 27.07,
+                "UYU": 43.3,
+                "ZAR": 18.78
+                </ul>
+                
+            </h1>
+   
 
         </div>
     )
