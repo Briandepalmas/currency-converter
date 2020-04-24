b5edbda05e1551ad2b42c8a1c696f9ace0684f0a
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-24 14:27:16 -0400

    remove rates component from app.js render

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -8,17 +8,14 @@ export default function Rates() {
     return (
         <div>
             <h1>
-                <ul>
+                <ul className="rates-list">
                  <li>"USD": 1</li>
                  <li>"AED": 3.67,</li>
                  <li>"BRL": 5.26,</li> 
                  <li>"GBP": 0.803,</li>
                  <li>"JPY": 107.68,</li>
-                </ul>
-                
+                </ul>        
             </h1>
-   
-
         </div>
     )
 }
