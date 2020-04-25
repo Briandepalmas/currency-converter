307a138af524afbd82b3adccb9df07da3c217ea9
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-24 22:34:32 -0400

    finished writing comments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Footer() {
-  
+  //signed and dated 
     return (
         <footer id="footer">BrianDepalmas © 2020, New York City.</footer>
     )
